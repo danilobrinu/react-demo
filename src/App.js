9b2e8e953b0b0d3c74beea95ff931b6205933a57
danilobrinu <ddbn.c2@gmail.com>
2019-06-17 17:00:19 -0500

    WIP

--- src/App.js
+++ src/App.js
@@ -1,28 +1,122 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+import { AppStateProvider, useAppState } from "./sweatpants";
+
+///////////////////////////////////////////////////////////////////////////////
+
+const initialState = {
+  todos: [{ id: 1, text: "8 O Clock" }],
+  visibility: "ALL"
+};
+
+const appStateReducer = (state = {}, action) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      return { ...state, todos: [...state.todos, action.todo] };
+    case "TOGGLE_TODO":
+      return {
+        ...state,
+        todos: state.todos.map(todo => {
+          if (todo.id === action.todo.id) {
+            return { ...todo, completed: !todo.completed };
+          }
+
+          return todo;
+        })
+      };
+    case "CHANGE_VISIBILITY":
+      return { ...state, visibility: action.visibility };
+    default:
+      return state;
   }
-}
+};
 
-export default App;
+///////////////////////////////////////////////////////////////////////////////
+
+const Todo = ({ todo }) => {
+  const [, dispatch] = useAppState();
+
+  return (
+    <li
+      onClick={() => dispatch({ type: "TOGGLE_TODO", todo })}
+      style={{
+        textDecoration: todo.completed ? "line-through" : "none"
+      }}
+    >
+      {todo.text}
+    </li>
+  );
+};
+
+const TodoList = ({ todos }) => (
+  <ul>
+    {todos.map(todo => (
+      <Todo key={todo.id} todo={todo} />
+    ))}
+  </ul>
+);
+
+const TodoFilters = () => {
+  const [, dispatch] = useAppState();
+
+  return (
+    <Fragment>
+      <button
+        onClick={() =>
+          dispatch({ type: "CHANGE_VISIBILITY", visibility: "ALL" })
+        }
+      >
+        Show All
+      </button>
+      <button
+        onClick={() =>
+          dispatch({ type: "CHANGE_VISIBILITY", visibility: "ACTIVE" })
+        }
+      >
+        Show Active
+      </button>
+      <button
+        onClick={() =>
+          dispatch({ type: "CHANGE_VISIBILITY", visibility: "COMPLETED" })
+        }
+      >
+        Show Completed
+      </button>
+    </Fragment>
+  );
+};
+
+const filterTodos = (todos, visibility) => {
+  switch (visibility) {
+    case "ACTIVE":
+      return todos.filter(todo => !todo.completed);
+    case "COMPLETED":
+      return todos.filter(todo => todo.completed);
+    case "ALL":
+    default:
+      return todos;
+  }
+};
+
+const TodoApp = () => {
+  const [state,] = useAppState();
+  const todos = filterTodos(state.todos, state.visibility);
+
+  return (
+    <Fragment>
+      <TodoList todos={todos} />
+      <TodoFilters />
+    </Fragment>
+  );
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+export default function App() {
+  return (
+    <AppStateProvider reducer={appStateReducer} initialState={initialState}>
+      <TodoApp />
+    </AppStateProvider>
+  );
+}
