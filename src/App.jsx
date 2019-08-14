31693785a4348f9efaeb4e47efa8a08203202aef
danilobrinu <ddbn.c2@gmail.com>
2019-08-14 16:53:37 -0500

    Minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,19 @@ import { AppStateProvider, useAppState } from "./sweatpants";
 ///////////////////////////////////////////////////////////////////////////////
 
 const initialState = {
-  todos: [
-    { id: 1, text: "Hacer la tarea temprano" },
-    { id: 2, text: "Comprar pan" }
-  ],
+  todos: [{ id: 1, text: "8 O Clock" }],
   visibility: "ALL"
 };
 
 const appStateReducer = (state = {}, action) => {
   switch (action.type) {
     case "ADD_TODO":
-      return { ...state, todos: [...state.todos, action.payload] };
+      return { ...state, todos: [...state.todos, action.todo] };
     case "TOGGLE_TODO":
       return {
         ...state,
         todos: state.todos.map(todo => {
-          if (todo.id === action.payload.id) {
+          if (todo.id === action.todo.id) {
             return { ...todo, completed: !todo.completed };
           }
 
@@ -29,7 +26,7 @@ const appStateReducer = (state = {}, action) => {
         })
       };
     case "CHANGE_VISIBILITY":
-      return { ...state, visibility: action.payload };
+      return { ...state, visibility: action.visibility };
     default:
       return state;
   }
@@ -42,10 +39,8 @@ const Todo = ({ todo }) => {
 
   return (
     <li
-      onClick={() => dispatch({ type: "TOGGLE_TODO", payload: todo })}
+      onClick={() => dispatch({ type: "TOGGLE_TODO", todo })}
       style={{
-        margin: 8,
-        cursor: "pointer",
         textDecoration: todo.completed ? "line-through" : "none"
       }}
     >
@@ -63,37 +58,27 @@ const TodoList = ({ todos }) => (
 );
 
 const TodoFilters = () => {
-  const [state, dispatch] = useAppState();
+  const [, dispatch] = useAppState();
 
   return (
     <Fragment>
       <button
-        style={{
-          color: "white",
-          backgroundColor: state.visibility === "ALL" ? "green" : "blue"
-        }}
-        onClick={() => dispatch({ type: "CHANGE_VISIBILITY", payload: "ALL" })}
+        onClick={() =>
+          dispatch({ type: "CHANGE_VISIBILITY", visibility: "ALL" })
+        }
       >
         Show All
       </button>
       <button
-        style={{
-          color: "white",
-          backgroundColor: state.visibility === "ACTIVE" ? "green" : "blue"
-        }}
         onClick={() =>
-          dispatch({ type: "CHANGE_VISIBILITY", payload: "ACTIVE" })
+          dispatch({ type: "CHANGE_VISIBILITY", visibility: "ACTIVE" })
         }
       >
         Show Active
       </button>
       <button
-        style={{
-          color: "white",
-          backgroundColor: state.visibility === "COMPLETED" ? "green" : "blue"
-        }}
         onClick={() =>
-          dispatch({ type: "CHANGE_VISIBILITY", payload: "COMPLETED" })
+          dispatch({ type: "CHANGE_VISIBILITY", visibility: "COMPLETED" })
         }
       >
         Show Completed
@@ -115,13 +100,12 @@ const filterTodos = (todos, visibility) => {
 };
 
 const TodoApp = () => {
-  const [state] = useAppState();
+  const [state,] = useAppState();
   const todos = filterTodos(state.todos, state.visibility);
 
   return (
     <Fragment>
-      {todos.length ? <TodoList todos={todos} /> : <h3>Add some todo's</h3>}
-
+      <TodoList todos={todos} />
       <TodoFilters />
     </Fragment>
   );
